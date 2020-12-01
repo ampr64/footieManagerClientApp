9bf7c26d09486448581b0b9778fc01fb514dc62f
Álvaro Prieto <alviprieto@hotmail.com>
2020-11-30 23:17:56 -0300

    Fix stadium property name in IClub.

--- src/app/clubs/models/iclub.ts
+++ src/app/clubs/models/iclub.ts
@@ -1,12 +1,15 @@
+import { ICoach } from 'src/app/coaches/models/icoach';
+import { IStadium } from 'src/app/stadiums/models/istadium';
+
 export interface IClub {
-    id?: number;
-    name?: string | null;
+    id: number;
+    name: string;
     president?: string | null;
-    stadium?: StadiumDto;
+    stadium: IStadium;
     leagueId?: number;
     yearFounded?: number;
     trophyCount?: number;
     badgeImageUrl?: string | null;
     squadCount?: number;
-    coach?: CoachDto;
+    coach?: ICoach;
 }
